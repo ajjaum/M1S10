a3c552827b035136ec9df54fb4c6bf96c015d288
ajjaum <j061701@dac.unicamp.br>
2024-04-10 11:09:10 -0300

    Iniciando ativida m1s10.

---
+++ src/test/java/br/com/fullStack/education/M1S10/M1S10ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullStack.education.M1S10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
